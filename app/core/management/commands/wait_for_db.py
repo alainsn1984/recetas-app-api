50646738946aecff4489013558fc483efc6901b2
Alain Serrano Naranjo <alainsn1984@gmail.com>
2024-06-11 17:50:01 +0200

    updated

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,4 +8,16 @@ class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self):
+        """Handle the command"""
         pass
+        # self.stdout.write('Waiting for database...')
+        # db_conn = None
+        # while not db_conn:
+        #     try:
+        #         db_conn = connections['default']
+        #     except OperationalError:
+        #
+        #         self.stdout.write('Database unavailable, waiting 1 second...')
+        #         time.sleep(1)
+        #
+        #     self.stdout.write(self.style.SUCCESS('Database available!'))
