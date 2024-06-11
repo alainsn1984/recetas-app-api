2b9f908f3249f06bdee0e91ae0615ef0e2ce2ce2
Alain Serrano Naranjo <alainsn1984@gmail.com>
2024-06-11 20:04:58 +0200

    feat:Updated docker compose file

--- app/core/tests/test_command.py
+++ app/core/tests/test_command.py
@@ -11,7 +11,7 @@ from django.test import SimpleTestCase
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class ComamandTest(SimpleTestCase):
+class CommandTests(SimpleTestCase):
     """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
@@ -20,4 +20,15 @@ class ComamandTest(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, time_sleep, patched_check):
+        """Test waiting for db when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+                                    [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
